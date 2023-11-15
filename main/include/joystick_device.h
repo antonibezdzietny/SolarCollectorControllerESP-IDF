44fd639bf6506887be425c0ac96c91addd364440
Antoni Bezdzietny <249327@student.pwr.edu.pl>
2023-11-15 23:09:55 +0100

    Created joystick_device

---
+++ main/include/joystick_device.h
@@ -0,0 +1,48 @@
+#ifndef JOYSTICK_DEVICE_H
+#define JOYSTICK_DEVICE_H
+
+#include "driver/gpio.h"
+#include "esp_adc/adc_oneshot.h"
+
+typedef struct
+{
+    gpio_num_t pin;
+    adc_unit_t unit;
+    adc_channel_t channel;
+    adc_oneshot_unit_handle_t handle;
+} joystick_device_adc_t;
+
+typedef struct
+{
+    gpio_num_t sw_pin;
+    joystick_device_adc_t y_adc_config;
+    joystick_device_adc_t x_adc_config;
+} joystick_device_config_t;
+
+typedef struct
+{
+    gpio_isr_t handle;
+    void *handle_arg;
+} joystick_device_handle_t;
+
+typedef struct
+{
+    joystick_device_config_t config;
+    joystick_device_handle_t sw_handle;
+} joystick_device_t;
+
+/* Public interfaces for Joystick */
+
+/* Init structure by passing config*/
+void joystick_device_init(joystick_device_t **self,
+                          joystick_device_config_t *joystick_device_config);
+/* Free memory and disable isr*/
+void joystick_device_deinit(joystick_device_t *self);
+/* Init isr for sw handle IMPORTANT: Before use gpio_install_isr_service()*/
+void joystick_device_set_isr(joystick_device_t *self,
+                             joystick_device_handle_t *joystick_device_handle);
+/* Get joystick x value*/
+int joystick_device_get_x(joystick_device_t *self);
+/* Get joystick y value*/
+int joystick_device_get_y(joystick_device_t *self);
+#endif
---
+++ main/src/joystick_device.c
@@ -0,0 +1,110 @@
+#include "include/joystick_device.h"
+
+static void _joystick_device_sw_config(joystick_device_t *self);
+static void _joystick_device_adc_config(joystick_device_t *self);
+static void _joystick_device_gpio_config(joystick_device_t *self);
+
+void joystick_device_init(joystick_device_t **self,
+                          joystick_device_config_t *joystick_device_config)
+{
+    (*self) = (joystick_device_t *)calloc(1, sizeof(joystick_device_t));
+
+    (*self)->config = *joystick_device_config;
+
+    _joystick_device_gpio_config((*self));
+}
+
+void joystick_device_deinit(joystick_device_t *self)
+{
+    if (!self->sw_handle.handle)
+        ESP_ERROR_CHECK_WITHOUT_ABORT(gpio_isr_handler_remove(self->config.sw_pin));
+    free(self);
+}
+
+void joystick_device_set_isr(joystick_device_t *self,
+                             joystick_device_handle_t *joystick_sw_handle)
+{
+    self->sw_handle = *joystick_sw_handle; // Be careful copy void *handle_arg
+    ESP_ERROR_CHECK_WITHOUT_ABORT(gpio_isr_handler_add(self->config.sw_pin,
+                                                       self->sw_handle.handle,
+                                                       self->sw_handle.handle_arg));
+}
+
+int joystick_device_get_x(joystick_device_t *self)
+{
+    int value = 0;
+    ESP_ERROR_CHECK_WITHOUT_ABORT(adc_oneshot_read(self->config.x_adc_config.handle,
+                                                   self->config.x_adc_config.channel,
+                                                   &value));
+    return value;
+}
+
+int joystick_device_get_y(joystick_device_t *self)
+{
+    int value = 0;
+    ESP_ERROR_CHECK_WITHOUT_ABORT(adc_oneshot_read(self->config.y_adc_config.handle,
+                                                   self->config.y_adc_config.channel,
+                                                   &value));
+    return value;
+}
+
+static void _joystick_device_sw_config(joystick_device_t *self)
+{
+    gpio_config_t config_sw = {
+        .pin_bit_mask = 1ULL << self->config.sw_pin,
+        .mode = GPIO_MODE_INPUT,
+        .pull_up_en = GPIO_PULLUP_ENABLE,
+        .pull_down_en = GPIO_PULLDOWN_DISABLE,
+        .intr_type = GPIO_INTR_NEGEDGE, // Interrupt on Rising Edge
+    };
+
+    gpio_config(&config_sw);
+}
+
+static void _joystick_device_adc_config(joystick_device_t *self)
+{
+    adc_oneshot_chan_cfg_t adc_config = {
+        // Config for both chanel
+        .atten = ADC_ATTEN_DB_11,
+        .bitwidth = ADC_BITWIDTH_11,
+    };
+
+    /* Initialization VRX ADC pin*/
+    adc_oneshot_unit_init_cfg_t config_vrx = {
+        .unit_id = self->config.x_adc_config.unit,
+        .ulp_mode = ADC_ULP_MODE_DISABLE,
+    };
+
+    adc_oneshot_new_unit(&config_vrx,
+                         &self->config.x_adc_config.handle);
+
+    adc_oneshot_config_channel(self->config.x_adc_config.handle,
+                               self->config.x_adc_config.channel,
+                               &adc_config);
+
+    /* Initialization VRY ADC pin*/
+    if (self->config.x_adc_config.unit != self->config.y_adc_config.unit)
+    {
+        adc_oneshot_unit_init_cfg_t config_vry = {
+            .unit_id = self->config.y_adc_config.unit,
+            .ulp_mode = ADC_ULP_MODE_DISABLE,
+        };
+
+        adc_oneshot_new_unit(&config_vry,
+                             &self->config.y_adc_config.handle);
+    }
+    else
+    {
+        self->config.y_adc_config.handle = self->config.x_adc_config.handle;
+    }
+
+    adc_oneshot_config_channel(self->config.y_adc_config.handle,
+                               self->config.y_adc_config.channel,
+                               &adc_config);
+}
+
+static void _joystick_device_gpio_config(joystick_device_t *self)
+{
+    _joystick_device_sw_config(self);
+    _joystick_device_adc_config(self);
+}
